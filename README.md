9580b3b7aca9cb8edde0300ac1a22ac7bbf2026f
Corey Alix <calix@dataworksplus.com>
2023-11-13 12:43:20 -0500

    implements most basic keyword search

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# keyword-search
-keyword-search
+# Overview
+This is a keyword-search proof-of-concept utility that builds an index for a single text document.
+Users can provide a search phrase, and those words will be searched for in the document.
+The best match(es) will be shown to the user, along with the line number(s) where the match(es) occurred.
+
+# Usage
+To run the program, execute the following command:
+```
+node keyword-search.js <search phrase>
+```
+
+# Algorithm
+
+The algorithm is very simple.  First we build an index and then we perform the search.
+
+## Build the Index
+1. Read the document line-by-line.
+2. For each line, split the line into words.
+3. For each word, add the line number to the index for that word.
+
+# Search
+
+1. Split the search phrase into keywords
+2. For each keyword, get the line numbers from the index
+3. Find the line number(s) that appear the most frequently
+4. Display the line number(s) and the line(s) of text
---
+++ keyword-search.js
@@ -0,0 +1,95 @@
+// 1. read the test1.txt file in the data folder
+// 2. build a keyword search index
+// 3. save the index to a file
+// 4. read the index from the file
+// 5. search the index for a list of keywords
+// 6. print the results to the console
+
+// 1. read the test1.txt file in the data folder
+const fs = require('fs');
+const path = require('path');
+const { argv } = require('process');
+const dataDir = path.join(__dirname, 'data');
+const fileName = 'test2.txt';
+const filePath = path.join(dataDir, fileName);
+const fileContents = fs.readFileSync(filePath, 'utf-8');
+
+// 2. build a keyword search index
+const index = buildIndex(fileContents);
+
+// 3. save the index to a file
+const indexFilePath = path.join(dataDir, `${fileName}.json`);
+fs.writeFileSync(indexFilePath, JSON.stringify(index));
+
+// 4. read the index from the file
+const indexContents = fs.readFileSync(indexFilePath, 'utf-8');
+const indexFromFile = JSON.parse(indexContents);
+
+// 5. search the index for a list of keywords
+const keywords = argv.slice(2);
+const searchResult = searchIndex(indexFromFile, keywords);
+const bestLine = findBestResult(searchResult);
+
+// 6. print the results to the console
+if (bestLine) {
+    console.log(bestLine);
+    const lines = fileContents.split('\n');
+    const previousLine = Math.max(1, bestLine - 1);
+    const nextLine = Math.min(lines.length, bestLine + 1);
+    for (let i = previousLine; i <= nextLine; i++) {
+        const line = lines[i - 1];
+        console.log(`${i}: ${line}`);
+    }
+} else {
+    console.log('No results found');
+}
+
+function buildIndex(fileContents) {
+    const lines = fileContents.split('\n');
+    const index = {};
+    for (let lineNumber = 0; lineNumber < lines.length; lineNumber++) {
+        const line = lines[lineNumber];
+        const words = line.split(' ');
+        for (let j = 0; j < words.length; j++) {
+            const word = words[j].toUpperCase();
+            if (!index[word]) {
+                index[word] = [];
+            }
+            index[word].push(lineNumber + 1);
+        }
+    }
+    return index;
+}
+
+function searchIndex(index, keywords) {
+    const results = {};
+    for (let i = 0; i < keywords.length; i++) {
+        const keyword = keywords[i].toUpperCase();
+        const lines = index[keyword];
+        if (lines) {
+            lines.forEach(line => {
+                if (line > 0) {
+                    results[line - 1] = (results[line - 1] || 0) + 1;
+                }
+                if (line < lines.length - 1) {
+                    results[line + 1] = (results[line + 1] || 0) + 1;
+                }
+                results[line] = (results[line] || 0) + 2;
+            })
+        }
+    }
+    return results;
+}
+
+function findBestResult(results) {
+    let bestResult = 0;
+    let bestLine = 0;
+    for (let line in results) {
+        const count = results[line];
+        if (count > bestResult) {
+            bestResult = count;
+            bestLine = parseInt(line);
+        }
+    }
+    return bestLine;
+}
