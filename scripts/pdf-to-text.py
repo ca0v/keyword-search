a04b7767f5b55067922aa0fb0d86bfb7b93a3a82
Corey Alix <calix@dataworksplus.com>
2023-11-15 09:55:40 -0500

    Switch from PyPDF2 to pdfplumber for better pdf->txt result

--- scripts/pdf-to-text.py
+++ scripts/pdf-to-text.py
@@ -5,16 +5,8 @@ import sys
 
 pdfFile = sys.argv[1]
 
-# convert the pdf to text
-from PyPDF2 import PdfReader
+import pdfplumber
 
-reader = PdfReader(pdfFile)
-
-# print every page
-pageNumber = 1
-for page in reader.pages:
-    # print page number
-    # print("Page", pageNumber)
-    pageNumber += 1
-    print(page.extract_text())
-    
+with pdfplumber.open(pdfFile) as reader:
+    for page in reader.pages:
+        print(page.extract_text())
