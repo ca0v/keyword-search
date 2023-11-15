cb748a229fd925bc7b5ddbab81b1ea1111a2d62a
Corey Alix <calix@dataworksplus.com>
2023-11-15 09:22:49 -0500

    wip:automating pdf->facts

---
+++ test.sh
@@ -0,0 +1,9 @@
+node keyword-search.js How do I compare a Probe Image to another image?
+node keyword-search.js How can I find my recent Facial Recognition Sessions?
+node keyword-search.js What is the difference between Lineups and Investigations?
+node keyword-search.js How do I filter my session results?
+node keyword-search.js What does Bookmarking an image do?
+node keyword-search.js Why is the search similar option not avaiable?
+node keyword-search.js Can I add more than 8 images to a lineup?
+node keyword-search.js Do changes I make in the image editor overwrite the original image?
+node keyword-search.js How can I correct the pose of my Mugshot?
